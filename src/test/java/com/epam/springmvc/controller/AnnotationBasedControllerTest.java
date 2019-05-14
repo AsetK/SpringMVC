deecf04589ffa89b8ed88732a7dda9645f61015d
Asset Kenezhanov <bysskc@mail.ru>
2019-05-14 16:01:01 +0600

    7-Commit: Mockito MockMvc

---
+++ src/test/java/com/epam/springmvc/controller/AnnotationBasedControllerTest.java
@@ -0,0 +1,37 @@
+package com.epam.springmvc.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AnnotationBasedControllerTest {
+
+    @InjectMocks
+    private AnnotationBasedController controller;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void someMethod1() throws Exception {
+
+        this.mockMvc.perform(get("/test/someaction1")).andExpect(status().isOk());
+
+    }
+
+
+}
